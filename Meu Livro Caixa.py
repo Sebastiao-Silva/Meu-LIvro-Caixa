19341928294fe8b9758039536f8dfe83ff7fb8aa
Sebastiao Silva <109110300+Sebastiao-Silva@users.noreply.github.com>
2026-03-06 21:39:05 -0300

    Update Meu Livro Caixa.py

--- Meu Livro Caixa.py
+++ Meu Livro Caixa.py
@@ -15,8 +15,8 @@ st.markdown("""
     
     /* Centralização Absoluta do Logo no Topo */
     [data-testid="stImage"] {
-        display: flex;
-        margin-left: 60%;
+        display: block;
+        margin-left: auto;
         margin-right: auto;
         width: fit-content;
     }
@@ -281,6 +281,7 @@ else:
                 df_v_del = df_v_del[df_v_del['ID'] != row['ID']]
                 df_v_del.to_csv(DB_VENDAS, index=False)
                 st.rerun()
+
 
 
 
